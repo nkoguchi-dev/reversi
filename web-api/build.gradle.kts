81fcaca20c409d4c444ae001e467a7d24e6402bf
Noriyuki KOGUCHI <noriyuki.koguchi@icloud.com>
2024-04-14 14:21:58 +0900

    最新バージョンを利用するよう修正

--- web-api/build.gradle.kts
+++ web-api/build.gradle.kts
@@ -1,36 +1,44 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompilationTask
 
 plugins {
-	id("org.springframework.boot") version "3.2.4"
-	id("io.spring.dependency-management") version "1.1.4"
-	kotlin("jvm") version "1.9.23"
-	kotlin("plugin.spring") version "1.9.23"
+    id("org.springframework.boot") version "3.2.4"
+    id("io.spring.dependency-management") version "1.1.4"
+    kotlin("jvm") version "2.0.0-RC1"
+    kotlin("plugin.spring") version "2.0.0-RC1"
 }
 
 group = "org.koppepan.demo"
 version = "0.0.1-SNAPSHOT"
 
 java {
-	sourceCompatibility = JavaVersion.VERSION_21
+    sourceCompatibility = JavaVersion.VERSION_21
 }
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+
+    // PostgreSQL + flyway + mybatis
+//	implementation("org.postgresql:postgresql")
+//	runtimeOnly("org.flywaydb:flyway-core")
+//	implementation("org.mybatis.spring.boot:mybatis-spring-boot-starter:3.0.3")
+
+    // テスト
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
-tasks.withType<KotlinCompile> {
-	kotlinOptions {
-		freeCompilerArgs += "-Xjsr305=strict"
-		jvmTarget = "21"
-	}
+tasks.named("compileKotlin", KotlinCompilationTask::class.java) {
+    compilerOptions {
+        freeCompilerArgs.add("-Xexport-kdoc")
+    }
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+    useJUnitPlatform()
 }
