a10c6a882afcc7ee7908c9c4a14877edadd215fb
Noriyuki KOGUCHI <noriyuki.koguchi@icloud.com>
2024-05-18 14:24:57 +0900

    SquarePositionに隣接するマスを返すメソッドを作成

--- web-api/src/main/kotlin/org/koppepan/demo/webapi/domain/board/SquarePositionModel.kt
+++ web-api/src/main/kotlin/org/koppepan/demo/webapi/domain/board/SquarePositionModel.kt
@@ -1,32 +1,43 @@
 package org.koppepan.demo.webapi.domain.board
 
-import org.koppepan.demo.webapi.domain.shared.CustomExceptionMessage
-import org.koppepan.demo.webapi.domain.shared.requireOrThrow
-
 /**
  * マス目の座標を表すクラス
  */
-class SquarePosition private constructor(
-    val x: Int,
-    val y: Int,
+class SquarePosition(
+    val x: HorizontalPosition,
+    val y: VerticalPosition,
 ) {
-    init {
-        requireOrThrow(x in 0..7) {
-            CustomExceptionMessage(
-                message = "SquarePositionのxは0から7の範囲で設定してください",
-                description = "",
-            )
+    fun getAdjacentPositions(): List<SquarePosition> {
+        val adjacentPositions = mutableListOf<SquarePosition>()
+        val prevX = x.prev()
+        val nextX = x.next()
+        val prevY = y.prev()
+        val nextY = y.next()
+        prevX?.let { px ->
+            prevY?.let { py ->
+                adjacentPositions.add(SquarePosition(px, py))
+            }
+            adjacentPositions.add(SquarePosition(px, y))
+            nextY?.let { ny ->
+                adjacentPositions.add(SquarePosition(px, ny))
+            }
         }
-        requireOrThrow(y in 0..7) {
-            CustomExceptionMessage(
-                message = "SquarePositionのyは0から7の範囲で設定してください",
-                description = "",
-            )
+        prevY?.let { py ->
+            adjacentPositions.add(SquarePosition(x, py))
         }
-    }
-
-    companion object {
-        fun create(x: Int, y: Int): SquarePosition = SquarePosition(x, y)
+        nextY?.let { ny ->
+            adjacentPositions.add(SquarePosition(x, ny))
+        }
+        nextX?.let { nx ->
+            prevY?.let { py ->
+                adjacentPositions.add(SquarePosition(nx, py))
+            }
+            adjacentPositions.add(SquarePosition(nx, y))
+            nextY?.let { ny ->
+                adjacentPositions.add(SquarePosition(nx, ny))
+            }
+        }
+        return adjacentPositions.toList()
     }
 
     override fun equals(other: Any?): Boolean {
@@ -43,8 +54,64 @@ class SquarePosition private constructor(
     }
 
     override fun hashCode(): Int {
-        var result = x
-        result = 31 * result + y
+        var result = x.value
+        result = 31 * result + y.value
         return result
+    }
+
+    override fun toString(): String {
+        return "($x, $y)"
+    }
+}
+
+enum class HorizontalPosition(val value: Int, val prev: Int?, val next: Int?) {
+    ONE(1, null, 2),
+    TWO(2, 1, 3),
+    THREE(3, 2, 4),
+    FOUR(4, 3, 5),
+    FIVE(5, 4, 6),
+    SIX(6, 5, 7),
+    SEVEN(7, 6, 8),
+    EIGHT(8, 7, null),
+    ;
+
+    companion object {
+        fun valueOf(value: Int?): HorizontalPosition? {
+            return HorizontalPosition.entries.find { it.value == value }
+        }
+    }
+
+    fun prev(): HorizontalPosition? {
+        return prev?.let { HorizontalPosition.valueOf(it) }
+    }
+
+    fun next(): HorizontalPosition? {
+        return next?.let { HorizontalPosition.valueOf(it) }
+    }
+}
+
+enum class VerticalPosition(val value: Int, val prev: Int?, val next: Int?) {
+    ONE(1, null, 2),
+    TWO(2, 1, 3),
+    THREE(3, 2, 4),
+    FOUR(4, 3, 5),
+    FIVE(5, 4, 6),
+    SIX(6, 5, 7),
+    SEVEN(7, 6, 8),
+    EIGHT(8, 7, null),
+    ;
+
+    companion object {
+        fun valueOf(value: Int?): VerticalPosition? {
+            return VerticalPosition.entries.find { it.value == value }
+        }
+    }
+
+    fun prev(): VerticalPosition? {
+        return prev?.let { VerticalPosition.valueOf(it) }
+    }
+
+    fun next(): VerticalPosition? {
+        return next?.let { VerticalPosition.valueOf(it) }
     }
 }
---
+++ web-api/src/test/kotlin/org/koppepan/demo/webapi/domain/board/SquarePositionTest.kt
@@ -0,0 +1,137 @@
+package org.koppepan.demo.webapi.domain.board
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+
+class SquarePositionTest {
+    @Test
+    @DisplayName("SquarePositionを作成する事ができる")
+    fun testCreate() {
+        val squarePosition = SquarePosition(HorizontalPosition.FOUR, VerticalPosition.FOUR)
+        assertEquals(HorizontalPosition.FOUR, squarePosition.x)
+        assertEquals(VerticalPosition.FOUR, squarePosition.y)
+    }
+
+    @Test
+    @DisplayName("SquarePosition同士が等しいかどうかを判定する事ができる")
+    fun testEquals() {
+        val squarePosition1 = SquarePosition(HorizontalPosition.FOUR, VerticalPosition.FOUR)
+        val squarePosition2 = SquarePosition(HorizontalPosition.FOUR, VerticalPosition.FOUR)
+        val squarePosition3 = SquarePosition(HorizontalPosition.FIVE, VerticalPosition.FIVE)
+        assertEquals(squarePosition1, squarePosition2)
+        assertNotEquals(squarePosition1, squarePosition3)
+    }
+
+    @Test
+    @DisplayName("SquarePositionの隣接する位置を取得する事ができる")
+    fun testGetAdjacentPositions() {
+        val squarePosition = SquarePosition(HorizontalPosition.FOUR, VerticalPosition.FOUR)
+        val adjacentPositions = squarePosition.getAdjacentPositions()
+        assertEquals(8, adjacentPositions.size)
+        assertTrue(adjacentPositions.contains(SquarePosition(HorizontalPosition.THREE, VerticalPosition.THREE)))
+        assertTrue(adjacentPositions.contains(SquarePosition(HorizontalPosition.FOUR, VerticalPosition.THREE)))
+        assertTrue(adjacentPositions.contains(SquarePosition(HorizontalPosition.FIVE, VerticalPosition.THREE)))
+        assertTrue(adjacentPositions.contains(SquarePosition(HorizontalPosition.THREE, VerticalPosition.FOUR)))
+        assertTrue(adjacentPositions.contains(SquarePosition(HorizontalPosition.FIVE, VerticalPosition.FOUR)))
+        assertTrue(adjacentPositions.contains(SquarePosition(HorizontalPosition.THREE, VerticalPosition.FIVE)))
+        assertTrue(adjacentPositions.contains(SquarePosition(HorizontalPosition.FOUR, VerticalPosition.FIVE)))
+        assertTrue(adjacentPositions.contains(SquarePosition(HorizontalPosition.FIVE, VerticalPosition.FIVE)))
+    }
+
+    @Test
+    @DisplayName("盤の左隅を表すSquarePositionの隣接する位置を取得する事ができる")
+    fun testGetAdjacentPositionsAtLeftMiddle() {
+        val squarePosition = SquarePosition(HorizontalPosition.ONE, VerticalPosition.FOUR)
+        val adjacentPositions = squarePosition.getAdjacentPositions()
+        assertEquals(5, adjacentPositions.size)
+        assertTrue(adjacentPositions.contains(SquarePosition(HorizontalPosition.ONE, VerticalPosition.THREE)))
+        assertTrue(adjacentPositions.contains(SquarePosition(HorizontalPosition.ONE, VerticalPosition.FIVE)))
+        assertTrue(adjacentPositions.contains(SquarePosition(HorizontalPosition.TWO, VerticalPosition.THREE)))
+        assertTrue(adjacentPositions.contains(SquarePosition(HorizontalPosition.TWO, VerticalPosition.FOUR)))
+        assertTrue(adjacentPositions.contains(SquarePosition(HorizontalPosition.TWO, VerticalPosition.FIVE)))
+    }
+
+    @Test
+    @DisplayName("盤の右隅を表すSquarePositionの隣接する位置を取得する事ができる")
+    fun testGetAdjacentPositionsAtRightMiddle() {
+        val squarePosition = SquarePosition(HorizontalPosition.EIGHT, VerticalPosition.FOUR)
+        val adjacentPositions = squarePosition.getAdjacentPositions()
+        assertEquals(5, adjacentPositions.size)
+        assertTrue(adjacentPositions.contains(SquarePosition(HorizontalPosition.EIGHT, VerticalPosition.THREE)))
+        assertTrue(adjacentPositions.contains(SquarePosition(HorizontalPosition.EIGHT, VerticalPosition.FIVE)))
+        assertTrue(adjacentPositions.contains(SquarePosition(HorizontalPosition.SEVEN, VerticalPosition.THREE)))
+        assertTrue(adjacentPositions.contains(SquarePosition(HorizontalPosition.SEVEN, VerticalPosition.FOUR)))
+        assertTrue(adjacentPositions.contains(SquarePosition(HorizontalPosition.SEVEN, VerticalPosition.FIVE)))
+    }
+
+    @Test
+    @DisplayName("盤の上隅を表すSquarePositionの隣接する位置を取得する事ができる")
+    fun testGetAdjacentPositionsAtTopMiddle() {
+        val squarePosition = SquarePosition(HorizontalPosition.FOUR, VerticalPosition.ONE)
+        val adjacentPositions = squarePosition.getAdjacentPositions()
+        assertEquals(5, adjacentPositions.size)
+        assertTrue(adjacentPositions.contains(SquarePosition(HorizontalPosition.THREE, VerticalPosition.ONE)))
+        assertTrue(adjacentPositions.contains(SquarePosition(HorizontalPosition.FIVE, VerticalPosition.ONE)))
+        assertTrue(adjacentPositions.contains(SquarePosition(HorizontalPosition.THREE, VerticalPosition.TWO)))
+        assertTrue(adjacentPositions.contains(SquarePosition(HorizontalPosition.FOUR, VerticalPosition.TWO)))
+        assertTrue(adjacentPositions.contains(SquarePosition(HorizontalPosition.FIVE, VerticalPosition.TWO)))
+    }
+
+    @Test
+    @DisplayName("盤の下隅を表すSquarePositionの隣接する位置を取得する事ができる")
+    fun testGetAdjacentPositionsAtBottomMiddle() {
+        val squarePosition = SquarePosition(HorizontalPosition.FOUR, VerticalPosition.EIGHT)
+        val adjacentPositions = squarePosition.getAdjacentPositions()
+        assertEquals(5, adjacentPositions.size)
+        assertTrue(adjacentPositions.contains(SquarePosition(HorizontalPosition.THREE, VerticalPosition.EIGHT)))
+        assertTrue(adjacentPositions.contains(SquarePosition(HorizontalPosition.FIVE, VerticalPosition.EIGHT)))
+        assertTrue(adjacentPositions.contains(SquarePosition(HorizontalPosition.THREE, VerticalPosition.SEVEN)))
+        assertTrue(adjacentPositions.contains(SquarePosition(HorizontalPosition.FOUR, VerticalPosition.SEVEN)))
+        assertTrue(adjacentPositions.contains(SquarePosition(HorizontalPosition.FIVE, VerticalPosition.SEVEN)))
+    }
+
+    @Test
+    @DisplayName("盤の左上角を表すSquarePositionの隣接する位置を取得する事ができる")
+    fun testGetAdjacentPositionsAtTopLeft() {
+        val squarePosition = SquarePosition(HorizontalPosition.ONE, VerticalPosition.ONE)
+        val adjacentPositions = squarePosition.getAdjacentPositions()
+        assertEquals(3, adjacentPositions.size)
+        assertTrue(adjacentPositions.contains(SquarePosition(HorizontalPosition.ONE, VerticalPosition.TWO)))
+        assertTrue(adjacentPositions.contains(SquarePosition(HorizontalPosition.TWO, VerticalPosition.ONE)))
+        assertTrue(adjacentPositions.contains(SquarePosition(HorizontalPosition.TWO, VerticalPosition.TWO)))
+    }
+
+    @Test
+    @DisplayName("盤の右上角を表すSquarePositionの隣接する位置を取得する事ができる")
+    fun testGetAdjacentPositionsAtTopRight() {
+        val squarePosition = SquarePosition(HorizontalPosition.EIGHT, VerticalPosition.ONE)
+        val adjacentPositions = squarePosition.getAdjacentPositions()
+        assertEquals(3, adjacentPositions.size)
+        assertTrue(adjacentPositions.contains(SquarePosition(HorizontalPosition.EIGHT, VerticalPosition.TWO)))
+        assertTrue(adjacentPositions.contains(SquarePosition(HorizontalPosition.SEVEN, VerticalPosition.ONE)))
+        assertTrue(adjacentPositions.contains(SquarePosition(HorizontalPosition.SEVEN, VerticalPosition.TWO)))
+    }
+
+    @Test
+    @DisplayName("盤の左下角を表すSquarePositionの隣接する位置を取得する事ができる")
+    fun testGetAdjacentPositionsAtBottomLeft() {
+        val squarePosition = SquarePosition(HorizontalPosition.ONE, VerticalPosition.EIGHT)
+        val adjacentPositions = squarePosition.getAdjacentPositions()
+        assertEquals(3, adjacentPositions.size)
+        assertTrue(adjacentPositions.contains(SquarePosition(HorizontalPosition.ONE, VerticalPosition.SEVEN)))
+        assertTrue(adjacentPositions.contains(SquarePosition(HorizontalPosition.TWO, VerticalPosition.EIGHT)))
+        assertTrue(adjacentPositions.contains(SquarePosition(HorizontalPosition.TWO, VerticalPosition.SEVEN)))
+    }
+
+    @Test
+    @DisplayName("盤の右下角を表すSquarePositionの隣接する位置を取得する事ができる")
+    fun testGetAdjacentPositionsAtBottomRight() {
+        val squarePosition = SquarePosition(HorizontalPosition.EIGHT, VerticalPosition.EIGHT)
+        val adjacentPositions = squarePosition.getAdjacentPositions()
+        assertEquals(3, adjacentPositions.size)
+        assertTrue(adjacentPositions.contains(SquarePosition(HorizontalPosition.EIGHT, VerticalPosition.SEVEN)))
+        assertTrue(adjacentPositions.contains(SquarePosition(HorizontalPosition.SEVEN, VerticalPosition.EIGHT)))
+        assertTrue(adjacentPositions.contains(SquarePosition(HorizontalPosition.SEVEN, VerticalPosition.SEVEN)))
+    }
+}
