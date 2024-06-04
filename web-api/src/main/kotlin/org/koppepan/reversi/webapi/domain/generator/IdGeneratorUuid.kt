700e58b3ef6e9af91bfa443d926aaa8f7d04fefe
Noriyuki KOGUCHI <noriyuki.koguchi@icloud.com>
2024-06-04 21:38:02 +0900

    importを修正

--- web-api/src/main/kotlin/org/koppepan/reversi/webapi/domain/generator/IdGeneratorUuid.kt
+++ web-api/src/main/kotlin/org/koppepan/reversi/webapi/domain/generator/IdGeneratorUuid.kt
@@ -1,7 +1,9 @@
 package org.koppepan.reversi.webapi.domain.generator
 
+import java.util.*
+
 class IdGeneratorUuid : IdGenerator {
     override fun generate(): String {
-        return java.util.UUID.randomUUID().toString()
+        return UUID.randomUUID().toString()
     }
 }
