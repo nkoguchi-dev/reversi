74216a3399eea598cc7ffe2e39084d4567a8f9af
Noriyuki KOGUCHI <noriyuki.koguchi@icloud.com>
2024-06-07 19:29:55 +0900

    IntegrationTestの設定からPostgreSQLとTestContainersを削除

--- web-api/src/integrationTest/kotlin/org/koppepan/reversi/webapi/IntegrationTest.kt
+++ web-api/src/integrationTest/kotlin/org/koppepan/reversi/webapi/IntegrationTest.kt
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ActiveProfiles
-import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.context.junit.jupiter.SpringExtension
 import org.testcontainers.junit.jupiter.Testcontainers
 import kotlin.annotation.AnnotationRetention.RUNTIME
@@ -13,9 +12,8 @@ import kotlin.annotation.AnnotationTarget.CLASS
 @Retention(RUNTIME)
 @Target(CLASS)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@ExtendWith(SpringExtension::class, org.koppepan.reversi.webapi.PostgreSQLTestContainersExtension::class, org.koppepan.reversi.webapi.TimeZoneExtension::class)
+@ExtendWith(SpringExtension::class, TimeZoneExtension::class)
 @Testcontainers
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-@ContextConfiguration(initializers = [org.koppepan.reversi.webapi.PostgreSQLTestContainersExtension::class])
 @ActiveProfiles("integration-test")
 annotation class IntegrationTest
