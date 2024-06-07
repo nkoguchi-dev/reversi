dcde231b3677d645f10dbc902fd4be32c625c890
Noriyuki KOGUCHI <noriyuki.koguchi@icloud.com>
2024-06-07 21:11:40 +0900

    movesテーブルを作成

--- web-api/src/main/resources/db/migration/V2__Craete_Tables.sql
+++ web-api/src/main/resources/db/migration/V2__Craete_Tables.sql
@@ -1,13 +1,26 @@
--- テーブルの作成と権限設定
 CREATE TABLE reversi.games
 (
-    game_id   VARCHAR(64),
-    player1_name VARCHAR(100) NOT NULL,
-    player2_name VARCHAR(100) NOT NULL,
-    created_at TIMESTAMP WITH TIME ZONE NOT NULL,
-    updated_at TIMESTAMP WITH TIME ZONE NOT NULL,
-    PRIMARY KEY (game_id)
+    game_id      VARCHAR(64),
+    player1_name VARCHAR(100)             NOT NULL,
+    player2_name VARCHAR(100)             NOT NULL,
+    status       VARCHAR(20)              NOT NULL,
+    created_at   TIMESTAMP WITH TIME ZONE NOT NULL,
+    updated_at   TIMESTAMP WITH TIME ZONE NOT NULL,
+    PRIMARY KEY (game_id),
+    CHECK (status IN ('CREATED', 'IN_PROGRESS', 'FINISHED'))
 );
-
--- reversi_userにtest_tableに対する権限を付与
 GRANT ALL ON reversi.games TO reversi_user;
+
+CREATE TABLE reversi.moves
+(
+    move_id             VARCHAR(64),
+    game_id             VARCHAR(64)              NOT NULL,
+    player_number       VARCHAR(10)              NOT NULL,
+    horizontal_position VARCHAR(10)              NOT NULL,
+    vertical_position   VARCHAR(10)              NOT NULL,
+    created_at          TIMESTAMP WITH TIME ZONE NOT NULL,
+    updated_at          TIMESTAMP WITH TIME ZONE NOT NULL,
+    PRIMARY KEY (move_id),
+    FOREIGN KEY (game_id) REFERENCES reversi.games (game_id)
+);
+GRANT ALL ON reversi.moves TO reversi_user;
