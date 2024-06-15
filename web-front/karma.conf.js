3828dcc30d2a8ea4c6e3eb36a49ed8e7a2108e45
Noriyuki KOGUCHI <noriyuki.koguchi@icloud.com>
2024-06-15 09:07:46 +0900

    karma.conf.jsを作成

---
+++ web-front/karma.conf.js
@@ -0,0 +1,43 @@
+module.exports = function(config) {
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('@angular-devkit/build-angular/plugins/karma')
+    ],
+    client: {
+      clearContext: false
+    },
+    jasmineHtmlReporter: {
+      suppressAll: true
+    },
+    coverageIstanbulReporter: {
+      dir: require('path').join(__dirname, './coverage/my-angular-app'),
+      subdir: '.',
+      reporters: [
+        { type: 'html' },
+        { type: 'text-summary' }
+      ]
+    },
+    reporters: ['progress', 'kjhtml'],
+    files: [],
+    exclude: [],
+    preprocessors: {},
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    browsers: ['Chrome', 'ChromeHeadless'],
+    customLaunchers: {
+      ChromeHeadless: {
+        base: 'Edge',
+        flags: ['--headless', '--disable-gpu', '--no-sandbox']
+      }
+    },
+    singleRun: false,
+    concurrency: Infinity
+  })
+}
