ba359babb1cc8fa71afc708bcc57b2f508591ff6
Noriyuki KOGUCHI <noriyuki.koguchi@icloud.com>
2024-06-15 13:11:31 +0900

    web-frontの失敗しているテストを修正

--- web-front/src/app/features/game/pages/game/game.component.spec.ts
+++ web-front/src/app/features/game/pages/game/game.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { GameComponent } from './game.component';
+import {provideHttpClient} from "@angular/common/http";
 
 describe('GameComponent', () => {
   let component: GameComponent;
@@ -8,7 +9,8 @@ describe('GameComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [GameComponent]
+      imports: [GameComponent],
+      providers: [provideHttpClient()],
     })
     .compileComponents();
 
