b0abcfc090397048d9d40f2194d8e75cfe8667ca
Noriyuki KOGUCHI <noriyuki.koguchi@icloud.com>
2024-06-28 23:12:52 +0900

    ECSタスクのリビジョン指定を修正

--- infrastructure/ecs.tf
+++ infrastructure/ecs.tf
@@ -15,6 +15,11 @@ data "aws_ssm_parameter" "flyway_password" {
   name = "/reversi/web-api/flyway_password"
 }
 
+data "aws_ecs_task_definition" "reversi_task_definition" {
+  task_definition = aws_ecs_task_definition.ReversiWebApi.family
+}
+
+
 resource "aws_ecs_cluster" "ReversiWebApi" {
   name     = "ReversiWebApi"
   tags     = {}
@@ -29,7 +34,7 @@ resource "aws_ecs_cluster" "ReversiWebApi" {
 resource "aws_ecs_service" "reversi-web-api" {
   name                               = "reversi-web-api"
   cluster                            = aws_ecs_cluster.ReversiWebApi.id
-  task_definition                    = "${aws_ecs_task_definition.ReversiWebApi.family}:${aws_ecs_task_definition.ReversiWebApi.revision}"
+  task_definition                    = "${aws_ecs_task_definition.ReversiWebApi.family}:${max(aws_ecs_task_definition.ReversiWebApi.revision, data.aws_ecs_task_definition.reversi_task_definition.revision)}"
   deployment_maximum_percent         = 200
   deployment_minimum_healthy_percent = 100
   desired_count                      = 0
