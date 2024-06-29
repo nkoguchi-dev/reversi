c3b626bd99106c8dc7b4c434a37e01a87a6c0c0a
Noriyuki KOGUCHI <noriyuki.koguchi@icloud.com>
2024-06-29 23:17:14 +0900

    Revert "ヘルスチェック呼び出し時にログを出力するよう変更"
    This reverts commit 8c8fccc28a63d770744d2b6cdef1ced7c7205aca.

--- web-api/src/main/kotlin/org/koppepan/reversi/webapi/presentation/health/HealthController.kt
+++ web-api/src/main/kotlin/org/koppepan/reversi/webapi/presentation/health/HealthController.kt
@@ -1,19 +1,12 @@
 package org.koppepan.reversi.webapi.presentation.health
 
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class HealthController {
-    companion object {
-        val log: Logger = LoggerFactory.getLogger(this::class.java)
-    }
-
     @GetMapping("/health")
     suspend fun get(): String {
-        log.info("Health check")
         return "ok"
     }
 }
