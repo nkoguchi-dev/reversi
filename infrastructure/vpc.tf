45c16c7430d6d580d1582c7d10b8c47101902fc5
Noriyuki KOGUCHI <noriyuki.koguchi@icloud.com>
2024-06-30 14:10:46 +0900

    VPCの定義を修正

--- infrastructure/vpc.tf
+++ infrastructure/vpc.tf
@@ -1,40 +1,46 @@
-resource "aws_vpc" "reversi" {
+resource "aws_vpc" "reversi-vpc" {
   cidr_block = "10.0.0.0/16"
-}
-
-resource "aws_subnet" "public_1" {
-  vpc_id                  = aws_vpc.reversi.id
-  cidr_block              = "10.0.1.0/24"
-  map_public_ip_on_launch = true
-  availability_zone       = "ap-northeast-1a"
-}
-
-resource "aws_subnet" "public_2" {
-  vpc_id                  = aws_vpc.reversi.id
-  cidr_block              = "10.0.2.0/24"
-  map_public_ip_on_launch = true
-  availability_zone       = "ap-northeast-1c"
-}
-
-resource "aws_internet_gateway" "gw" {
-  vpc_id = aws_vpc.reversi.id
-}
-
-resource "aws_route_table" "public" {
-  vpc_id = aws_vpc.reversi.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.gw.id
+  tags     = {
+    "Name" = "reversi-vpc"
+  }
+  tags_all = {
+    "Name" = "reversi-vpc"
   }
 }
 
-resource "aws_route_table_association" "public_1" {
-  subnet_id      = aws_subnet.public_1.id
-  route_table_id = aws_route_table.public.id
-}
+#resource "aws_subnet" "public_1" {
+#  vpc_id                  = aws_vpc.reversi.id
+#  cidr_block              = "10.0.1.0/24"
+#  map_public_ip_on_launch = true
+#  availability_zone       = "ap-northeast-1a"
+#}
+#
+#resource "aws_subnet" "public_2" {
+#  vpc_id                  = aws_vpc.reversi.id
+#  cidr_block              = "10.0.2.0/24"
+#  map_public_ip_on_launch = true
+#  availability_zone       = "ap-northeast-1c"
+#}
+#
+#resource "aws_internet_gateway" "gw" {
+#  vpc_id = aws_vpc.reversi.id
+#}
+#
+#resource "aws_route_table" "public" {
+#  vpc_id = aws_vpc.reversi.id
+#
+#  route {
+#    cidr_block = "0.0.0.0/0"
+#    gateway_id = aws_internet_gateway.gw.id
+#  }
+#}
 
-resource "aws_route_table_association" "public_2" {
-  subnet_id      = aws_subnet.public_2.id
-  route_table_id = aws_route_table.public.id
-}
+#resource "aws_route_table_association" "public_1" {
+#  subnet_id      = aws_subnet.public_1.id
+#  route_table_id = aws_route_table.public.id
+#}
+#
+#resource "aws_route_table_association" "public_2" {
+#  subnet_id      = aws_subnet.public_2.id
+#  route_table_id = aws_route_table.public.id
+#}
