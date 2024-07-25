e0b4a603deeb6e030a1a92b07d8b22214eae3fd1
Noriyuki KOGUCHI <noriyuki.koguchi@icloud.com>
2024-07-26 06:53:44 +0900

    cloudfront.tfのフォーマットを修正

--- infrastructure/cloudfront.tf
+++ infrastructure/cloudfront.tf
@@ -1,60 +1,41 @@
+
+
 resource "aws_cloudfront_distribution" "reversi" {
-  aliases = [
-    "reversi.koppepan.org",
-  ]
+  aliases             = ["reversi.koppepan.org"]
   enabled             = true
-  default_root_object = "index.html"
   is_ipv6_enabled     = true
-  price_class         = "PriceClass_All"
   retain_on_delete    = false
+  default_root_object = "index.html"
+  price_class         = "PriceClass_All"
 
   default_cache_behavior {
-    allowed_methods = [
-      "GET",
-      "HEAD",
-    ]
-    cache_policy_id = "658327ea-f89d-4fab-a63d-7e88639e58f6"
-    cached_methods  = [
-      "GET",
-      "HEAD",
-    ]
+    allowed_methods        = ["GET", "HEAD"]
+    cached_methods         = ["GET", "HEAD"]
     compress               = true
     default_ttl            = 0
     max_ttl                = 0
     min_ttl                = 0
     smooth_streaming       = false
-    target_origin_id       = aws_s3_bucket.reversi-web-front.bucket_regional_domain_name
-    trusted_key_groups     = []
-    trusted_signers        = []
     viewer_protocol_policy = "allow-all"
+    cache_policy_id        = "658327ea-f89d-4fab-a63d-7e88639e58f6"
+    target_origin_id       = aws_s3_bucket.reversi-web-front.bucket_regional_domain_name
   }
 
   ordered_cache_behavior {
-    allowed_methods = [
-      "DELETE",
-      "GET",
-      "HEAD",
-      "OPTIONS",
-      "PATCH",
-      "POST",
-      "PUT",
-    ]
-    cache_policy_id = "4135ea2d-6df8-44a3-9df3-4b5a84be39ad"
-    cached_methods  = [
-      "GET",
-      "HEAD",
-    ]
+    allowed_methods          = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods           = ["GET", "HEAD"]
     compress                 = true
     default_ttl              = 0
     max_ttl                  = 0
     min_ttl                  = 0
-    origin_request_policy_id = "216adef6-5c7f-47e4-b989-5492eafa07d3"
-    path_pattern             = "/api/*"
     smooth_streaming         = false
-    target_origin_id         = aws_lb.reversi-web-api-lb.dns_name
+    path_pattern             = "/api/*"
     trusted_key_groups       = []
     trusted_signers          = []
+    target_origin_id         = aws_lb.reversi-web-api-lb.dns_name
+    cache_policy_id          = "4135ea2d-6df8-44a3-9df3-4b5a84be39ad"
     viewer_protocol_policy   = "redirect-to-https"
+    origin_request_policy_id = "216adef6-5c7f-47e4-b989-5492eafa07d3"
   }
 
   origin {
@@ -80,9 +61,7 @@ resource "aws_cloudfront_distribution" "reversi" {
       origin_keepalive_timeout = 5
       origin_protocol_policy   = "match-viewer"
       origin_read_timeout      = 30
-      origin_ssl_protocols     = [
-        "TLSv1.2",
-      ]
+      origin_ssl_protocols     = ["TLSv1.2"]
     }
   }
 
