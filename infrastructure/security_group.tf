9ed40bbc63c35e1d99b1a11a29668a4c59fe02f2
Noriyuki KOGUCHI <noriyuki.koguchi@icloud.com>
2024-07-29 18:20:35 +0900

    security_groupの設定を修正

--- infrastructure/security_group.tf
+++ infrastructure/security_group.tf
@@ -2,34 +2,30 @@ resource "aws_security_group" "ecs_service-sg" {
   name        = "reversi_ecs_service-sg"
   vpc_id      = aws_vpc.reversi-vpc.id
   description = "for_reversi_ecs_service_security_group"
-  egress      = [
-    {
-      cidr_blocks = [
-        "0.0.0.0/0",
-      ]
-      description      = ""
-      from_port        = 0
-      ipv6_cidr_blocks = []
-      prefix_list_ids  = []
-      protocol         = "-1"
-      security_groups  = []
-      self             = false
-      to_port          = 0
-    },
-  ]
   ingress = [
     {
-      cidr_blocks = [
-        "0.0.0.0/0",
-      ]
-      description      = ""
       from_port        = 8080
+      to_port          = 8080
+      cidr_blocks      = ["0.0.0.0/0"]
+      description      = ""
       ipv6_cidr_blocks = []
       prefix_list_ids  = []
       protocol         = "tcp"
       security_groups  = []
       self             = false
-      to_port          = 8080
+    },
+  ]
+  egress      = [
+    {
+      from_port        = 0
+      to_port          = 0
+      cidr_blocks      = ["0.0.0.0/0"]
+      description      = ""
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "-1"
+      security_groups  = []
+      self             = false
     },
   ]
   tags     = {}
@@ -39,37 +35,33 @@ resource "aws_security_group" "ecs_service-sg" {
 resource "aws_security_group" "reversi_lb-sg" {
   vpc_id      = aws_vpc.reversi-vpc.id
   name        = "reversi_lb-sg"
-  description = "for_reversi_load_barancer_security_group"
+  description = "for_reversi_load_balancer_security_group"
   tags        = {}
   tags_all    = {}
-  egress      = [
-    {
-      cidr_blocks = [
-        "0.0.0.0/0",
-      ]
-      description      = ""
-      from_port        = 0
-      ipv6_cidr_blocks = []
-      prefix_list_ids  = []
-      protocol         = "-1"
-      security_groups  = []
-      self             = false
-      to_port          = 0
-    },
-  ]
   ingress = [
     {
-      cidr_blocks = [
-        "0.0.0.0/0",
-      ]
-      description      = ""
       from_port        = 0
+      to_port          = 0
+      cidr_blocks      = ["0.0.0.0/0"]
+      description      = ""
       ipv6_cidr_blocks = []
       prefix_list_ids  = []
       protocol         = "-1"
       security_groups  = []
       self             = false
-      to_port          = 0
     }
+  ]
+  egress      = [
+    {
+      from_port        = 0
+      to_port          = 0
+      cidr_blocks      = ["0.0.0.0/0"]
+      description      = ""
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "-1"
+      security_groups  = []
+      self             = false
+    },
   ]
 }
