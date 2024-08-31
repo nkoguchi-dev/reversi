18bf7fa42b4fb37eaa9cb1d3a1ecbf7d6a9964c1
Noriyuki KOGUCHI <noriyuki.koguchi@icloud.com>
2024-08-31 13:59:23 +0900

    ゲーム開始ボタンの処理を追加

--- front/src/features/main_screen/MainScreen.tsx
+++ front/src/features/main_screen/MainScreen.tsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './MainScreen.css';
 import StartButton from './StartButton';
+import {GameStartRequest, GameStartResponse, GameStartService} from "./services/GameStartApiService";
+
+const gameStartService = new GameStartService();
 
 function MainScreen() {
-    const handleStartGame = () => {
-        console.log('ゲームが開始されました！');
-        // ゲームの初期化ロジックなどをここに追加
+    const [gameData, setGameData] = useState<GameStartResponse | null>(null);
+    const [error, setError] = useState<string | null>(null);
+    const handleStartGame = async () => {
+        const request: GameStartRequest = {
+            player1: 'Player 1',
+            player2: 'Player 2',
+        };
+        try {
+            const data = await gameStartService.startGame(request);
+            setGameData(data);
+        } catch (err) {
+            setError('ゲームの開始に失敗しました。');
+        }
     };
 
     return (
@@ -16,7 +29,16 @@ function MainScreen() {
                 詳細については、<a href="https://github.com/nkoguchi-dev/reversi" target="_blank">GitHubのリポジトリ</a>をご覧ください。
             </p>
             <br/>
-            <StartButton onClick={handleStartGame} />
+            <StartButton onClick={handleStartGame}/>
+            {error && <p>{error}</p>}
+            {gameData && (
+                <div>
+                    <h2>ゲームID: {gameData.gameId}</h2>
+                    <p>プレイヤー1: {gameData.player1Name}</p>
+                    <p>プレイヤー2: {gameData.player2Name}</p>
+                    <p>次のプレイヤー: {gameData.nextPlayer}</p>
+                </div>
+            )}
         </div>
     );
 }
