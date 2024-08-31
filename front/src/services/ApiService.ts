2f5c7bb8b533ddd8d3b5584d25cf2bc0f173c838
Noriyuki KOGUCHI <noriyuki.koguchi@icloud.com>
2024-08-31 14:06:29 +0900

    古いフロントエンドのアプリケーションを削除

--- front/src/services/ApiService.ts
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const apiClient = axios.create({
-    baseURL: process.env.REACT_APP_API_BASE_URL,
-    headers: {'Content-Type': 'application/json'},
-});
-
-export default apiClient;
