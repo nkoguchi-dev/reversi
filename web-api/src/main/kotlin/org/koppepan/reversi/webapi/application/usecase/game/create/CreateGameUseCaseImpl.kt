6c40c4f9f3142db2bdefda4c95cb7f0016112bfa
Noriyuki KOGUCHI <noriyuki.koguchi@icloud.com>
2024-09-16 12:35:45 +0900

    不要な処理を削除

--- web-api/src/main/kotlin/org/koppepan/reversi/webapi/application/usecase/game/create/CreateGameUseCaseImpl.kt
+++ web-api/src/main/kotlin/org/koppepan/reversi/webapi/application/usecase/game/create/CreateGameUseCaseImpl.kt
@@ -1,8 +1,7 @@
 package org.koppepan.reversi.webapi.application.usecase.game.create
 
-import org.komapper.r2dbc.R2dbcDatabase
-import org.koppepan.reversi.webapi.domain.game.SaveGameRepository
 import org.koppepan.reversi.webapi.domain.game.Game
+import org.koppepan.reversi.webapi.domain.game.SaveGameRepository
 import org.koppepan.reversi.webapi.domain.generator.IdGenerator
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
@@ -12,27 +11,19 @@ import org.springframework.stereotype.Service
 class CreateGameUseCaseImpl(
     private val idGenerator: IdGenerator,
     private val saveGameRepository: SaveGameRepository,
-    private val db: R2dbcDatabase,
 ) : CreateGameUseCase {
     companion object {
         val log: Logger = LoggerFactory.getLogger(this::class.java)
     }
 
     override suspend fun create(input: CreateGameUseCase.Input): CreateGameUseCase.Output {
-        val game = try {
-            Game.start(
-                idGenerator = idGenerator,
-                player1Name = input.player1Name,
-                player2Name = input.player2Name,
-            )
-        } catch (e: Exception) {
-            log.error("Gameの開始に失敗しました。", e)
-            throw e
-        }
+        val game = Game.start(
+            idGenerator = idGenerator,
+            player1Name = input.player1Name,
+            player2Name = input.player2Name,
+        )
 
-        db.withTransaction {
-            saveGameRepository.save(game)
-        }
+        saveGameRepository.save(game)
 
         log.info("Gameを開始しました。 game: $game")
         return CreateGameUseCase.Output(
